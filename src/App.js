67132e5cedae2abc0a034713b27db041c9a35932
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2022-10-13 19:50:15 -0400

    import state and effect, created array with variable and setter for useState, and useEffect with axios call for res.data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => {
+        console.log(res.data);
+      }).catch(err => console.error(err));
+  }, [])
+
   return (
     <div className="App">
-      <p>
+      {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      </p> */}
+
     </div>
   );
 }
