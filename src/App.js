d586a8b1486ffad1bde4d1ae1b5bf2c7c7fccf2f
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2022-10-14 09:40:31 -0400

    in App component returning NasaPhoto and props

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./App.css";
 
-const dummyData = {
-  date: "2022-10-13",
-  explanation: "What are those strange rings? Rich in dust, the rings are likely 3D shells -- but how they were created remains a topic of research.  Where they were created is well known: in a binary star system that lies about 6,000 light years away toward the constellation of the Swan (Cygnus) -- a system dominated by the Wolf-Rayet star WR 140.  Wolf-Rayet stars are massive, bright, and known for their tumultuous winds. They are also known for creating and dispersing heavy elements such as carbon  which is a building block of interstellar dust. The other star in the binary is also bright and massive -- but not as active.  The two great stars joust in an oblong orbit as they approach each other about every eight years. When at closest approach, the X-ray emission from the system increases, as, apparently, does the dust expelled into space -- creating another shell.  The featured infrared image by the new Webb Space Telescope resolves greater details and more dust shells than ever before.",
-  hdurl: "https://apod.nasa.gov/apod/image/2209/WR140_WebbSchmidt_960.jpg",
-  title: "Dust Shells around WR 140 from Webb"
-}
+import NasaPhoto from './Components/NasaPhoto';
+
+// const dummyData = {
+//   date: "2022-10-13",
+//   explanation: "What are those strange rings? Rich in dust, the rings are likely 3D shells -- but how they were created remains a topic of research.  Where they were created is well known: in a binary star system that lies about 6,000 light years away toward the constellation of the Swan (Cygnus) -- a system dominated by the Wolf-Rayet star WR 140.  Wolf-Rayet stars are massive, bright, and known for their tumultuous winds. They are also known for creating and dispersing heavy elements such as carbon  which is a building block of interstellar dust. The other star in the binary is also bright and massive -- but not as active.  The two great stars joust in an oblong orbit as they approach each other about every eight years. When at closest approach, the X-ray emission from the system increases, as, apparently, does the dust expelled into space -- creating another shell.  The featured infrared image by the new Webb Space Telescope resolves greater details and more dust shells than ever before.",
+//   hdurl: "https://apod.nasa.gov/apod/image/2209/WR140_WebbSchmidt_960.jpg",
+//   title: "Dust Shells around WR 140 from Webb"
+// }
 
 function App() {
-  const [data, setData] = useState(dummyData);
+  const [data, setData] = useState();
 
-  // useEffect(() => {
-  //   axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-  //     .then(res => {
-  //       console.log(res.data);
-  //     }).catch(err => console.error(err));
-  // }, [])
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => {
+        setData(res.data);
+      }).catch(err => console.error(err));
+  }, [])
 
   return (
     <div className="App">
@@ -25,7 +27,7 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p> */}
-
+      { data && <NasaPhoto photo={data} />}
     </div>
   );
 }
